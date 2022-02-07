0be51b22f010e323c4f3cd483abb919a20701fc8
Jackson R Wippel <jrwippel@hotmail.com>
2022-02-07 09:23:17 -0300

    User entity and resource

---
+++ src/main/java/com/estudojava/course/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.estudojava.course.resources;
+
+import com.estudojava.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User user = new User(1L, "Jackson", "jackson.wippel@ambevtech.com.br", "479999999", "123456");
+        return ResponseEntity.ok().body(user);
+
+    }
+
+
+}
