d4bed55b1211e0e612d96f101c2f6e17923fae7b
Jackson R Wippel <jrwippel@hotmail.com>
2022-02-11 07:48:04 -0300

    Payment, one-to-one association

--- src/main/java/com/estudojava/course/config/TestConfig.java
+++ src/main/java/com/estudojava/course/config/TestConfig.java
@@ -73,5 +73,9 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+        Payment payment = new Payment(null, Instant.parse("2019-06-20T20:30:02Z"), o1);
+        o1.setPayment(payment);
+        orderRepository.save(o1);
+
     }
 }
