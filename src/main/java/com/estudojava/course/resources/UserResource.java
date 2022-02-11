5b5ee26d229186fbfdbf92b77a2586913062cccb
Jackson R Wippel <jrwippel@hotmail.com>
2022-02-11 16:29:55 -0300

    User insert

--- src/main/java/com/estudojava/course/resources/UserResource.java
+++ src/main/java/com/estudojava/course/resources/UserResource.java
@@ -5,7 +5,10 @@ import com.estudojava.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.Servlet;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,6 +26,14 @@ public class UserResource {
     public ResponseEntity<User> finById(@PathVariable Long id){
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert (@RequestBody User obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+
     }
 
 
