b0a7cb81bb712f07e6879db1611fab5a21277e6b
Jackson R Wippel <jrwippel@hotmail.com>
2022-02-14 08:33:55 -0300

    Tratamento de exceção findById

--- src/main/java/com/estudojava/course/services/UserService.java
+++ src/main/java/com/estudojava/course/services/UserService.java
@@ -2,8 +2,10 @@ package com.estudojava.course.services;
 
 import com.estudojava.course.entities.User;
 import com.estudojava.course.repositories.UserRepository;
+import com.estudojava.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,9 +19,10 @@ public class UserService {
     public List<User> findAll(){
         return repository.findAll();
     }
+
     public User findById(Long id){
         Optional <User> optionalUser = repository.findById(id);
-        return optionalUser.get();
+        return optionalUser.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
