c075e376d3437a864af56246ef3b437c06f1b75e
Jackson R Wippel <jrwippel@hotmail.com>
2022-02-15 08:26:40 -0300

    Tratamento exceção delete

--- src/main/java/com/estudojava/course/services/UserService.java
+++ src/main/java/com/estudojava/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.estudojava.course.services;
 
 import com.estudojava.course.entities.User;
 import com.estudojava.course.repositories.UserRepository;
+import com.estudojava.course.services.exceptions.DatabaseException;
 import com.estudojava.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.ResourceAccessException;
 
@@ -30,6 +33,13 @@ public class UserService {
     }
 
     public void delete (Long id){
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+
     }
 }
